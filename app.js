f270c4490c4c8613ce781050436cc5040cd47d81
thedoritos <thedoritos@gmail.com>
2018-07-08 14:23:12 +0900

    Inject html

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-window.alert("Hello!!");
+const googleContainerId = "rcnt";
+const googleContainer = document.getElementById(googleContainerId)
+
+const appContainer = document.createElement("div");
+appContainer.setAttribute("class", "app-container");
+googleContainer.appendChild(appContainer);
+
+const appHtmlURL = chrome.runtime.getURL("app.html");
+fetch(appHtmlURL)
+    .then((response) => {
+        return response.text();
+    })
+    .then((html) => {
+        appContainer.innerHTML = html;
+    });
