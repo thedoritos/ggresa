f34ef292974be09abab4febeb82be03ab1284278
thedoritos <thedoritos@gmail.com>
2018-07-09 01:48:39 +0900

    Inject options & repositories

--- inject.js
+++ inject.js
@@ -5,22 +5,46 @@ const appContainer = document.createElement("div");
 appContainer.setAttribute("class", "app-container");
 googleContainer.appendChild(appContainer);
 
-const appHtmlURL = chrome.runtime.getURL("app.html");
-fetch(appHtmlURL)
-    .then((response) => {
-        return response.text();
+const fetchOptions = function() {
+    return new Promise((resolve) => {
+        chrome.storage.sync.get({
+            accessToken: "",
+            teamId: ""
+        }, (options) => {
+            resolve(options);
+        });
+    });
+};
+
+fetchOptions()
+    .then((options) => {
+        // Inject options.
+        Object.keys(options)
+            .forEach((key) => {
+                const meta = document.createElement("meta");
+                meta.setAttribute("name", key);
+                meta.setAttribute("content", options[key]);
+                document.head.appendChild(meta);
+            })
     })
+    .then(() => {
+        const appHtmlURL = chrome.runtime.getURL("app.html");
+        return fetch(appHtmlURL)
+    })
+    .then((response) => { return response.text(); })
     .then((html) => {
         appContainer.innerHTML = html;
 
-        // Executes scripts.
-        const appDiv = document.getElementById("app");
-        const appDivContainer = appDiv.parentElement;
-
+        // Inject scripts.
         const scriptSrcs = [
             "https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
+            chrome.runtime.getURL("repository/options_repository.js"),
+            chrome.runtime.getURL("repository/posts_repository.js"),
             chrome.runtime.getURL("app.js")
         ];
+
+        const appDiv = document.getElementById("app");
+        const appDivContainer = appDiv.parentElement;
 
         const injectScripts = (scriptSrcs) => {
             if (scriptSrcs.length === 0) { return; }
