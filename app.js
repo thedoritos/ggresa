3cb21c5fc7159016c62e99019c5f4f5652d85af1
thedoritos <thedoritos@gmail.com>
2018-07-08 17:09:35 +0900

    Show dummy results

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+Vue.component('esa-post', {
+    props: ['post'],
+    template: '<li>{{ post.title }}</li>'
+});
+
 new Vue({
     el: "#app",
     data: {
-        message: "Hello"
+        searchResults: []
+    },
+    methods: {
+        search: function() {
+            this.searchResults = [
+                { id: 0, title: "Android DataBinding Overview" },
+                { id: 1, title: "Spek: Test Your App Today" },
+                { id: 2, title: "RxAndroid is dead. Long live LiveData" }
+            ];
+        }
+    },
+    mounted: function() {
+        this.search();
     }
 });
